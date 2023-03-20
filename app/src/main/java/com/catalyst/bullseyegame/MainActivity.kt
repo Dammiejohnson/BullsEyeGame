0af0caee2cda242ff19f67b690d515fa95cfccc6
Oluwadamilola Sanni <Oluwadamilola.Sanni@Interswitchng.com>
2023-03-20 12:24:27 +0100

    implementing start over function and rounds

--- app/src/main/java/com/catalyst/bullseyegame/MainActivity.kt
+++ app/src/main/java/com/catalyst/bullseyegame/MainActivity.kt
@@ -11,21 +11,34 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private var sliderValue  = 0
-    private var targetValue = Random.nextInt(1, 100)
+    private var targetValue = newTargetValue()
+    private  var totalScore = 0
+    private  var currentRound = 1
+
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        supportActionBar?.hide() //temporary fix to hide the app bar
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        binding.targetTextView.text = targetValue.toString()
+        startNewGame()
 
         binding.btnHitMe.setOnClickListener {
             Log.i("Button Clicked Event", "You clicked the Hit me Button")
             showResult()
+
+            totalScore += pointsForCurrentRound()
+            binding.gameScoreTextView?.text = totalScore.toString()
+        }
+
+
+        binding.startOverButton?.setOnClickListener {
+            startNewGame()
         }
 
         binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
@@ -34,26 +47,47 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
 
         })
 
     }
 
-    private fun pointsForCurrentRound(): Int {
-      val maxScore = 100
-        val difference = abs(targetValue - sliderValue)
+    private fun differenceAmount() = abs(targetValue - sliderValue)
 
-        return maxScore - difference
+    private fun newTargetValue() = Random.nextInt(1, 100)
+
+    private fun pointsForCurrentRound(): Int {
+        val maxScore = 100
+        val difference = differenceAmount()
+        var bonus = 0
+
+        if(difference == 0){
+            bonus = 100
+        } else if (difference == 1){
+            bonus = 50
+        }
+        return maxScore - difference + bonus
+    }
+
+    private fun startNewGame() {
+        totalScore = 0
+        currentRound = 1
+        sliderValue = 50
+        targetValue = newTargetValue()
+
+        binding.targetTextView.text = targetValue.toString()
+        binding.gameRoundTextView?.text = currentRound.toString()
+        binding.gameScoreTextView?.text = totalScore.toString()
+        binding.seekBar.progress = sliderValue
+
+
     }
 
     private fun showResult() {
-        val dialogTitle = getString(R.string.result_dialog_title)
+        val dialogTitle = alertTitle()
         val dialogMessage =
             getString(R.string.result_dialog_message, sliderValue, pointsForCurrentRound())
-//        val dialogMessage = "The Slider's value is $sliderValue"
-
 
         val builder = AlertDialog.Builder(this)
 
@@ -61,9 +95,31 @@ class MainActivity : AppCompatActivity() {
         builder.setMessage(dialogMessage)
         builder.setPositiveButton(R.string.result_dialog_button_text) { dialog, _ ->
             dialog.dismiss()
+
+            targetValue = newTargetValue()
+            binding.targetTextView.text = targetValue.toString()
+
+            currentRound += 1
+            binding.gameRoundTextView?.text = currentRound.toString()
+
         }
 
         builder.create().show()
+    }
+
+    private fun alertTitle(): String {
+        val difference = differenceAmount()
+
+        val title: String = if (difference == 0 ) {
+            getString(R.string.alert_title_1)
+        }else if (difference < 5) {
+            getString(R.string.alert_title_2)
+        }else if (difference <= 10) {
+            getString(R.string.alert_title_3)
+        }else {
+            getString(R.string.alert_title_4)
+        }
+        return title
     }
 
 
